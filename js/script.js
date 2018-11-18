545212fe1c7ee7719b86442c0fcee873c9eae582
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-11-18 18:10:49 -0500

    added team cards and logo space

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// scrollspy
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.scrollspy', '.parallax');
+  var instances = M.ScrollSpy.init(elems, options);
+});
+
+// parallax
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.parallax');
+//   var instances = M.Parallax.init(elems, options);
+// });
